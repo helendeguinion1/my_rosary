17f121df2056b7038502143a0bb24a9039ef54d3
helendeguinion1 <helendeguinion@gmail.com>
2024-05-22 20:44:57 +0800

    Configure Firebase in flutter for Android and IOS - to add plist file in IOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 //Screens
 import 'package:my_rosary/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
